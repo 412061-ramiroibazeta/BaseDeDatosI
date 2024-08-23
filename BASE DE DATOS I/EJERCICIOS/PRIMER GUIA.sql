4c88160ca3270d3dac1d4ebd585ab7ab202f62ca
412061-ramiroibazeta <412061@tecnicatura.frc.utn.edu.ar>
2024-08-22 21:02:04 -0300

    Ejercicios de la clase 22-8

--- BASE DE DATOS I/EJERCICIOS/PRIMER GUIA.sql
+++ BASE DE DATOS I/EJERCICIOS/PRIMER GUIA.sql
@@ -168,4 +168,67 @@ GROUP BY c.nom_cliente
 HAVING count(*) > 30 or sum(df.cantidad * df.pre_unitario) > 800000
 order by 2
 
--- HACER EJERCICIOS DE PDF TUP_2C_BDI_GUIA_Repaso_Consultas
+-----------------------------------------------CLASE 22/08-------------------------------------------------
+
+-- Calcular el total facturado por cada vendedor y a cada cliente el año pasado ordenado por vendedor primero y luego por cliente
+
+SELECT SUM(df.pre_unitario * df.cantidad), 
+v.nom_vendedor 'Nombre de vendedor', 
+c.nom_cliente 'Nombre del cliente'
+FROM facturas f
+JOIN vendedores v ON f.cod_vendedor = v.cod_vendedor 
+JOIN detalle_facturas df ON f.nro_factura = df.nro_factura
+JOIN clientes c on f.cod_cliente = c.cod_cliente
+WHERE YEAR(f.fecha) = YEAR(getdate())-1
+GROUP BY v.nom_vendedor, c.nom_cliente
+ORDER BY 2,3
+
+-- Granuralidad de los datos - BUSCAR INFO
+
+-- 2 - Se necesita un listado que informe sobre el monto(individual por producto) máximo, mínimo y total que gastó en esta librería cada cliente el año pasado, pero solo donde el importe total gastado por esos clientes este entre 50000 y 90000
+
+SELECT
+c.nom_cliente 'Nombre del cliente',
+MIN(df.pre_unitario) 'Mínimo',
+MAX(df.pre_unitario) 'Máximo',
+SUM(df.pre_unitario * df.cantidad) 'Total'
+FROM Facturas f
+JOIN detalle_facturas df ON f.nro_factura = df.nro_factura
+JOIN clientes c on f.cod_cliente = c.cod_cliente
+WHERE YEAR(f.fecha) = YEAR(getdate())-1
+GROUP BY c.nom_cliente
+HAVING SUM(df.pre_unitario * df.cantidad) BETWEEN 50000 AND 90000
+
+--  FUNCIONES INCORPORADAS
+
+-- MATEMATICAS
+
+-- Desde la administración se solicita un reporte que muestre el precio promedio, el importe total y el promedio del importe vendido por articulo que no comience con 'c', 
+--y que ese importe total vendido sea superior a 20000. Redondear promedios hacia abajo e importes hacia arriba
+
+SELECT
+a.descripcion,
+FLOOR(avg(a.pre_unitario)) 'Promedio',
+CEILING(SUM(df.cantidad * a.pre_unitario)) 'Importe total',
+CEILING(AVG(df.cantidad * a.pre_unitario)) 'Promedio Total'
+FROM Facturas f
+JOIN detalle_facturas df ON f.nro_factura = df.nro_factura
+JOIN articulos a on a.cod_articulo = df.cod_articulo
+WHERE a.descripcion not like 'c%'
+GROUP BY a.descripcion
+HAVING SUM(a.pre_unitario * df.cantidad) > 20000
+
+-- FUNCIONES FECHA/HORA
+
+SELECT 
+SUM(df.pre_unitario * df.cantidad), 
+v.nom_vendedor 'Nombre de vendedor',
+c.nom_cliente 'Nombre del cliente',
+DATENAME(weekday,f.fecha) 'Día de la venta',
+DATENAME(year,f.fecha) 'Año'
+FROM facturas f
+JOIN vendedores v ON f.cod_vendedor = v.cod_vendedor 
+JOIN detalle_facturas df ON f.nro_factura = df.nro_factura
+JOIN clientes c on f.cod_cliente = c.cod_cliente
+GROUP BY v.nom_vendedor, c.nom_cliente, DATENAME(weekday,f.fecha), DATENAME(year,f.fecha)
+ORDER BY DATENAME(weekday,f.fecha)
