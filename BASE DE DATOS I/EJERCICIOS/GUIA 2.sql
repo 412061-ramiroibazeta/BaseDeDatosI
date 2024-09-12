581bf81653de4e787267f82482fba4d40beb0050
412061-ramiroibazeta <412061@tecnicatura.frc.utn.edu.ar>
2024-09-12 13:17:18 -0300

    Ejercicios de Subconsultas

--- BASE DE DATOS I/EJERCICIOS/GUIA 2.sql
+++ BASE DE DATOS I/EJERCICIOS/GUIA 2.sql
@@ -42,8 +42,35 @@ and a.cod_articulo NOT IN ( Select df.cod_articulo
 -- Se debería listar en la consulta principal, los vendedores con sus ventas totales agrupado por vendedor(codigo y nombre) y 
 -- como condición es que su venta total sea superior al promedio general de ventas.
 
-Select v.nom_vendedor, v.cod_vendedor, count(*)
+--Select v.nom_vendedor, v.cod_vendedor, count(*)
+--FROM facturas f
+--JOIN vendedores v on f.cod_vendedor = v.cod_vendedor
+--WHERE (Select 
+--GROUP BY v.nom_vendedor, v.cod_vendedor
+
+--- Se quiere saber que clientes ni vinieron entre el 12-12-2015 y 13-7-2020
+SET DATEFORMAT DMY
+Select c.cod_cliente 'Codigo', c.nom_cliente 'Nombre'
+FROM clientes C
+WHERE c.cod_cliente NOT IN (
+							select f.cod_cliente
+							From facturas f
+							Where f.fecha between '12-12-2015' AND '13-07-2020')
+
+--- Listar los datos de las facturas de los clientes que solo vienen a comprar en febrero es decir que todas las veces que compraron fueron en febrero y no en otro mes.
+
+Select c.cod_cliente 'Codigo', c.nom_cliente 'Nombre'
+From clientes c
+JOIN facturas f1 ON f1.cod_cliente = c.cod_cliente
+Where 2 = all (select Month(f.fecha) from facturas f where c.cod_cliente = f.cod_cliente )
+
+--- Realizar un listado de número de factura, fecha, cliente, articulo e importe para los casos en que al menoss uno de los importes de esa factura sea menor a 3000
+
+Select f.nro_factura 'Nro' , f.fecha 'Fecha', f.cod_cliente 'Cliente', SUM(df.pre_unitario*df.cantidad) 'Importe'
 FROM facturas f
-JOIN vendedores v on f.cod_vendedor = v.cod_vendedor
-WHERE (Select 
-GROUP BY v.nom_vendedor, v.cod_vendedor
+JOIN detalle_facturas df on f.nro_factura = df.nro_factura
+JOIN clientes c on c.cod_cliente = f.cod_cliente
+JOIN articulos a on df.cod_articulo = a.cod_articulo
+WHERE 3000 > any (
+					Select SUM(df1.pre_unitario*df1.cantidad) from detalle_facturas df1 WHERE df1.nro_factura = df.nro_factura)
+GROUP BY f.nro_factura , f.fecha, f.cod_cliente
